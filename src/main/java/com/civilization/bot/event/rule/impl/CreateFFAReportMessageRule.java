90064ac4b803be7cba42ba2dcdf0eba4e984612d
Yauheniy Lepkovich <evgeniy.lepkovich@gmail.com>
2020-05-27 20:35:47 +0300

    fix bug with ignoring names which contains '.'

--- src/main/java/com/civilization/bot/event/rule/impl/CreateFFAReportMessageRule.java
+++ src/main/java/com/civilization/bot/event/rule/impl/CreateFFAReportMessageRule.java
@@ -9,7 +9,7 @@ import java.util.function.Predicate;
 public class CreateFFAReportMessageRule implements MessageListenedAppliedRule {
     @Override
     public Predicate<String> getRule() {
-        return message -> (message.matches("[!]report\\s\\d+\\swinner:\\s@[A-Za-z0-9А-Яа-я\\(\\)\\_].+")
-                || message.matches("[!]репорт\\s\\d+\\sпобедитель:\\s@[A-Za-z0-9А-Яа-я\\(\\)\\_].+"));
+        return message -> (message.matches("[!]report\\s\\d+\\swinner:\\s@[A-Za-z0-9А-Яа-я\\(\\)\\_\\.].+")
+                || message.matches("[!]репорт\\s\\d+\\sпобедитель:\\s@[A-Za-z0-9А-Яа-я\\(\\)\\_\\.].+"));
     }
 }
